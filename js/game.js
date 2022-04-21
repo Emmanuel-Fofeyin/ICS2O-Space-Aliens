469ad4a857d07743c1ef3924b584555d702e7909
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-04-21 02:03:40 +0000

    added code in js/game.js and in index.html

--- js/game.js
+++ js/game.js
@@ -4,13 +4,26 @@
 //
 // Created by: Emmanuel Fofeyin
 // Created on: April 20 2022
+// This is a Phaser3 configuration file
 
-
+//*Game scene */
 const config = { 
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a 
+  physics: {
+    defult: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
