08a32993ab5cc54a4dbaf094c70b5457efef11c7
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-29 01:15:00 +0000

    added finishing touches 💋💋 ✨💎💎

--- js/gameScene.js
+++ js/gameScene.js
@@ -48,10 +48,10 @@ class GameScene extends Phaser.Scene {
     console.log("Game Scene")
 
     // images
-    this.load.image("starBackground", "./assets/starBackground.png")
-    this.load.image("ship", "./assets/spaceShip.png")
+    this.load.image("starBackground", "./assets/galaxy.png")
+    this.load.image("ship", "./assets/pod.png")
     this.load.image("missile", "./assets/missile.png")
-    this.load.image("alien", "assets/alien.png")
+    this.load.image("alien", "assets/alien-ship.png")
     // sound
     this.load.audio("laser", "./assets/laser1.wav")
     this.load.audio("explosion", "./assets/barrelExploding.wav")
